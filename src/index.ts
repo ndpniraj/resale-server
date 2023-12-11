4a7f7c9127c765a1a93318aa3e3be6089ddb23f4
Niraj Dhungana <fsniraj@gmail.com>
2023-12-11 16:13:01 +0530

    middleware function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
-const a = 5;
-const b = 5;
+import express, { RequestHandler } from "express";
 
-console.log(a + b);
+const app = express();
+
+// const bodyParser: RequestHandler = (req, res, next) => {
+//   req.on("data", (chunk) => {
+//     req.body = JSON.parse(chunk);
+//     next();
+//   });
+// };
+
+// app.use(bodyParser);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "This message is coming from server." });
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "This message is coming from post request." });
+});
+
+app.post("/create-new-product", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "This message is coming from product create." });
+});
+
+app.listen(8000, () => {
+  console.log("The app is running on http://localhost:8000");
+});
